763622c84666c59c19c24685045464e65f30ab6e
Reem Hassan <reemhassan742r@gmail.com>
2025-10-13 15:48:08 +0300

    APIs-DevCreed

---
+++ APIs/Fundamentals/SurveyBasket/SurveyBasket.Api/Controllers/DevelopmentController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SurveyBasket.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DevelopmentController : ControllerBase
+    {
+    }
+}
